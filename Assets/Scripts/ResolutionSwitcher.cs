4081ac2a18b5e1fa0fbb83231deed41d79cfdddc
Azirel <azirel92@gmail.com>
2019-07-31 19:40:00 +0300

    Init

---
+++ Assets/Scripts/ResolutionSwitcher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ResolutionSwitcher : MonoBehaviour
+{
+	public KeyCode SwitchResolutionKey = KeyCode.M;
+	public UIRoot UIRoot;
+
+	[ContextMenu("SwitchResolutions")]
+	public void SwapWidthWithHeight()
+	{
+		var temp = UIRoot.manualWidth;
+		UIRoot.manualWidth = UIRoot.manualHeight;
+		UIRoot.manualHeight = temp;
+		SetSize(UIRoot.manualWidth == 1080 ? 5 : 6);
+	}
+
+	public static void SetSize(int index)
+	{
+		var gvWndType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.GameView");
+		var selectedSizeIndexProp = gvWndType.GetProperty("selectedSizeIndex",
+				BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+		var gvWnd = UnityEditor.EditorWindow.GetWindow(gvWndType);
+		selectedSizeIndexProp.SetValue(gvWnd, index, null);
+	}
+}
